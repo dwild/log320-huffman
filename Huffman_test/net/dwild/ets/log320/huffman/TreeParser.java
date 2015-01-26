273cb805f0b0660c6f7d4ad94f1a5efc8d1901f1
Guillaume Voisine <guillaume.voisine@gmail.com>
2015-01-26 12:47:43 -0500

    Changement d'accents dans un commentaire. (Sinon ça ne fonctionne pas sur ma machine)

--- Huffman_test/net/dwild/ets/log320/huffman/TreeParser.java
+++ Huffman_test/net/dwild/ets/log320/huffman/TreeParser.java
@@ -3,42 +3,42 @@ package net.dwild.ets.log320.huffman;
 import java.util.ArrayList;
 
 public class TreeParser {
-	private ArrayList<Boolean> bufferStructure = new ArrayList<Boolean>();
-	private ArrayList<Byte> bufferContent = new ArrayList<Byte>();
-	
-	public TreeParser() { }
-	
-	/**
-	 * Permet de parcourir une seule fois l'arbre à la place de deux fois avec l'ancienne méthode
-	 * @param node
-	 */
-	public void writeTree(TreeNode node) {
+ private ArrayList<Boolean> bufferStructure = new ArrayList<Boolean>();
+ private ArrayList<Byte> bufferContent = new ArrayList<Byte>();
+ 
+ public TreeParser() { }
+ 
+ /**
+  * Permet de parcourir une seule fois l'arbre Ã  la place de deux fois avec l'ancienne mÃ©thode
+  * @param node
+  */
+ public void writeTree(TreeNode node) {
         ITreeNode left = node.getLeft();
         if(left instanceof TreeNodeValue) {
-        	this.bufferStructure.add(true);
-        	this.bufferContent.add(((TreeNodeValue) left).getValue());
+         this.bufferStructure.add(true);
+         this.bufferContent.add(((TreeNodeValue) left).getValue());
         }
         else if(left instanceof TreeNode) {
-        	this.bufferStructure.add(false);
-        	this.writeTree((TreeNode) left);
+         this.bufferStructure.add(false);
+         this.writeTree((TreeNode) left);
         }
 
         ITreeNode right = node.getRight();
         if(right instanceof TreeNodeValue) {
-        	this.bufferStructure.add(true);
-        	this.bufferContent.add(((TreeNodeValue) right).getValue());
+         this.bufferStructure.add(true);
+         this.bufferContent.add(((TreeNodeValue) right).getValue());
         }
         else if(right instanceof TreeNode) {
-        	this.bufferStructure.add(false);
-        	this.writeTree((TreeNode) right);
+         this.bufferStructure.add(false);
+         this.writeTree((TreeNode) right);
         }
-	}
-	
-	public ArrayList<Boolean> getBufferStructure() {
-		return bufferStructure;
-	}
-	
-	public ArrayList<Byte> getBufferContent() {
-		return bufferContent;
-	}
+ }
+ 
+ public ArrayList<Boolean> getBufferStructure() {
+  return bufferStructure;
+ }
+ 
+ public ArrayList<Byte> getBufferContent() {
+  return bufferContent;
+ }
 }
